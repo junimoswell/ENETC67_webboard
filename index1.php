9da9f2a46614b21e0a22c600b1ee2e106a862de1
junimoswell <junimoswell.le@gmail.com>
2024-08-06 11:19:53 +0700

    Initial commit

---
+++ index1.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Webboard</title>
+</head>
+<body>
+    <h1 style="text-align: center;">Webboard</h1>
+    <hr>
+    <form >
+    หมวดหมู่: <select name="category">
+        <option value="all">ทั้งหมด</option>
+        <option value="general">เรื่องทั่วไป</option>
+        <option value="study">เรื่องเรียน</option>
+    </select>
+   <?php 
+         echo "<a href='login1.php' style='float: right;'>เข้าสู่ระบบ</a>";
+     ?>
+    
+</form>
+
+<ul> 
+    <?php 
+    for ($i = 1; $i <= 10 ;$i++ ){
+        echo "<li><a href='post1.php?id=$i'>กระทู้ที่ $i</a></li>";
+        }     
+    ?>
+    
+
+</ul>
+
+</body>
+</html>
